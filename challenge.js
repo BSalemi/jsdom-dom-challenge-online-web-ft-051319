94855cc05e32895dc07d80c565c804b4dbeb3d35
Brian <briandsalemi@gmail.com>
2019-08-18 14:08:00 -0400

    Done.

--- challenge.js
+++ challenge.js
@@ -71,10 +71,26 @@ let commentInput = document.querySelector('input');
             }
         });
 
-      likeBtn.addEventListener('click', function(e){
+        // 1. Create an li whose initial value is going to be 1 on first click
+        // 2. If clicked again, check if ul has child node inside if so grab node/check value.
+
+        let likesChild = document.querySelector('.likes')
+
         const taskLi = document.createElement("li");
-        document.querySelector(".likes").append(taskLi);
-        let NumLikes = document.getElementsByTagName('li').length 
-        return NumLikes
-        
-      })
+        function appendLikes(){
+            
+            if(likesChild.innerHTML != null) {
+                likesChild = parseInt(likesChild.innerHTML) + 1 ;
+                // likesChild.innerHTML = likesTotal;
+         } else {
+
+                taskLi.innerText = 0; 
+                taskLi.innerText = parseInt(taskLi.innerText) + 1
+                document.querySelector(".likes").append(taskLi); 
+        } }
+
+
+        likeBtn.addEventListener('click', appendLikes)  
+           
+ 
+      
